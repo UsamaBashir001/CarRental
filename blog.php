746c00e522ea6b8b3415561dd6384bb1522b62e9
Usama Bashir <131294811+UsamaBashir001@users.noreply.github.com>
2023-06-25 00:49:51 -0700

    Add files via upload

---
+++ blog.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+  <title>Car Rental Blog</title>
+</head>
+
+<body>
+  <div class="container">
+    <div class="row">
+        <div class="col-lg-12">
+            <h1>WAS SYSTEM</h1>
+            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero blanditiis accusantium illo ea, iste tenetur excepturi pariatur esse reiciendis dolor nobis aliquid qui autem vitae voluptatibus vero dignissimos impedit inventore fugiat, cupiditate quam maiores error. Tempore quasi porro sequi voluptate, at veritatis doloribus illum fuga sit. Nesciunt nam tempore, quibusdam molestias ex laborum doloribus et dolorum accusantium rem possimus. Nisi dolore sint quisquam in voluptatum veniam, temporibus placeat aspernatur qui ipsum praesentium minus suscipit. Est repellat debitis cumque ex architecto ab odio. Assumenda, deleniti molestiae numquam optio esse repellendus ipsum quam libero aspernatur consequatur illo totam quae eaque eos vitae!</p>
+            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero blanditiis accusantium illo ea, iste tenetur excepturi pariatur esse reiciendis dolor nobis aliquid qui autem vitae voluptatibus vero dignissimos impedit inventore fugiat, cupiditate quam maiores error. Tempore quasi porro sequi voluptate, at veritatis doloribus illum fuga sit. Nesciunt nam tempore, quibusdam molestias ex laborum doloribus et dolorum accusantium rem possimus. Nisi dolore sint quisquam in voluptatum veniam, temporibus placeat aspernatur qui ipsum praesentium minus suscipit. Est repellat debitis cumque ex architecto ab odio. Assumenda, deleniti molestiae numquam optio esse repellendus ipsum quam libero aspernatur consequatur illo totam quae eaque eos vitae!</p>
+            <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero blanditiis accusantium illo ea, iste tenetur excepturi pariatur esse reiciendis dolor nobis aliquid qui autem vitae voluptatibus vero dignissimos impedit inventore fugiat, cupiditate quam maiores error. Tempore quasi porro sequi voluptate, at veritatis doloribus illum fuga sit. Nesciunt nam tempore, quibusdam molestias ex laborum doloribus et dolorum accusantium rem possimus. Nisi dolore sint quisquam in voluptatum veniam, temporibus placeat aspernatur qui ipsum praesentium minus suscipit. Est repellat debitis cumque ex architecto ab odio. Assumenda, deleniti molestiae numquam optio esse repellendus ipsum quam libero aspernatur consequatur illo totam quae eaque eos vitae!</p>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-6">
+            <img src="cars/civic.png" alt="">
+        </div>
+        <div class="col-lg-6 mt-5">
+            <h1>Civic</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, totam a laudantium ratione aperiam laboriosam exercitationem consectetur. Odit ipsam a, eum veniam praesentium magni id est quibusdam in neque autem sit ab qui accusantium explicabo pariatur, exercitationem velit sunt nisi, laudantium ea porro alias. Ducimus eius perspiciatis maxime in laudantium rem ab! Fugiat sunt iure aut ex exercitationem commodi, quidem in unde ab, maxime excepturi? Optio unde ad quaerat explicabo praesentium tempore nam corrupti cupiditate ut, animi nobis tenetur modi provident! Eveniet amet deleniti sit repellendus provident rem accusantium ipsa veniam distinctio facere ab doloribus culpa earum blanditiis, quo, mollitia, repudiandae doloremque laboriosam inventore. Sequi illo repellendus incidunt dicta dolorem hic officia tempore delenit</p>
+        </div>
+        <div class="col-lg-6">
+            
+            <h1>corola</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, totam a laudantium ratione aperiam laboriosam exercitationem consectetur. Odit ipsam a, eum veniam praesentium magni id est quibusdam in neque autem sit ab qui accusantium explicabo pariatur, exercitationem velit sunt nisi, laudantium ea porro alias. Ducimus eius perspiciatis maxime in laudantium rem ab! Fugiat sunt iure aut ex exercitationem commodi, quidem in unde ab, maxime excepturi? Optio unde ad quaerat explicabo praesentium tempore nam corrupti cupiditate ut, animi nobis tenetur modi provident! Eveniet amet deleniti sit repellendus provident rem accusantium ipsa veniam distinctio facere ab doloribus culpa earum blanditiis, quo, mollitia, repudiandae doloremque laboriosam inventore. Sequi illo repellendus incidunt dicta dolorem hic officia tempore delenit</p>
+        </div>
+        <div class="col-lg-6">
+            <img src="cars/corola.png" alt="" class="w-100">
+        </div>
+        <div class="col-lg-6">
+            <img src="cars/fortuner.png " alt="" class="w-100">
+        </div>
+        <div class="col-lg-6">
+            <h1>fortuner</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, totam a laudantium ratione aperiam laboriosam exercitationem consectetur. Odit ipsam a, eum veniam praesentium magni id est quibusdam in neque autem sit ab qui accusantium explicabo pariatur, exercitationem velit sunt nisi, laudantium ea porro alias. Ducimus eius perspiciatis maxime in laudantium rem ab! Fugiat sunt iure aut ex exercitationem commodi, quidem in unde ab, maxime excepturi? Optio unde ad quaerat explicabo praesentium tempore nam corrupti cupiditate ut, animi nobis tenetur modi provident! Eveniet amet deleniti sit repellendus provident rem accusantium ipsa veniam distinctio facere ab doloribus culpa earum blanditiis, quo, mollitia, repudiandae doloremque laboriosam inventore. Sequi illo repellendus incidunt dicta dolorem hic officia tempore delenit</p>
+        </div>
+        <div class="col-lg-6">
+            
+            <h1>Hilux</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates, totam a laudantium ratione aperiam laboriosam exercitationem consectetur. Odit ipsam a, eum veniam praesentium magni id est quibusdam in neque autem sit ab qui accusantium explicabo pariatur, exercitationem velit sunt nisi, laudantium ea porro alias. Ducimus eius perspiciatis maxime in laudantium rem ab! Fugiat sunt iure aut ex exercitationem commodi, quidem in unde ab, maxime excepturi? Optio unde ad quaerat explicabo praesentium tempore nam corrupti cupiditate ut, animi nobis tenetur modi provident! Eveniet amet deleniti sit repellendus provident rem accusantium ipsa veniam distinctio facere ab doloribus culpa earum blanditiis, quo, mollitia, repudiandae doloremque laboriosam inventore. Sequi illo repellendus incidunt dicta dolorem hic officia tempore delenit</p>
+        </div>
+        <div class="col-lg-6 mt-5">
+            <img src="cars/hilux.png" alt="" class="w-100">
+        </div>
+        <div class="row">
+            <div class="col-lg-2 mx-auto">
+                <a href="" class="btn btn-lg btn-outline-info">Read More</a>
+            </div>
+          </div>
+    </div>
+  </div>
+ 
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
